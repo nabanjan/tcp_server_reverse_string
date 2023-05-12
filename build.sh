d24b5142eb178743c1d3609b0299e8c6ed4e728e
Nabanjan Das <nabanjan@gmail.com>
2023-05-12 10:45:22 +0000

    Updated

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ clear
 #sudo apt-get install g++
 #sudo apt-get install libssl-dev
 #sudo apt-get install libpoco-dev
+#sudo ufw allow 28888
+#sudo fuser 28888/tcp
 g++ -std=c++14 ./tcpserver.cpp -o tcpserver -lPocoFoundation -lPocoNet -lPocoUtil 
--- tcpserver.cpp
+++ tcpserver.cpp
@@ -1,4 +1,9 @@
+#include <signal.h>
+
 #include <iostream>
+#include <chrono>
+#include <ctime>   
+
 #include "Poco/Net/TCPServer.h"
 #include "Poco/Net/TCPServerConnection.h"
 #include "Poco/Net/TCPServerConnectionFactory.h"
@@ -6,7 +11,11 @@
 
 using namespace std;
 using namespace Poco;
-using namespace Net;
+using namespace Poco::Net;
+
+
+
+TCPServer *server;
 
 class ReverseStringConnection: public TCPServerConnection {
 public:
@@ -18,6 +27,8 @@ public:
       char *buffer = new char[256];
       buffer[256] = {0};
       int c = sock.receiveBytes(buffer, sizeof(buffer));
+      time_t start_time;
+      time_t end_time;
       while (c > 0) {
         string data(buffer);
         reverse(data.begin(), data.end());
@@ -26,38 +37,50 @@ public:
         buffer = new char[256];
         buffer[256] = {0};
 	delete [] temp_buffer;
+	start_time = time(NULL);
         c = sock.receiveBytes(buffer, sizeof(buffer));
+	end_time = time(NULL);
       }
-    } catch (Exception& exc) { 
-      cerr << "ReverseStringConnection: " << exc.displayText() << endl;
+    } catch (Exception& e) { 
+      cerr << "ReverseStringConnection: " << e.displayText() << endl;
+      if (server != nullptr) {
+        server->stop();
+      }
     }
   }
 };
 
-int main() {
-    int portNo = 28888;
-    TCPServer server(new TCPServerConnectionFactoryImpl<ReverseStringConnection>(), portNo);
-    server.start();
-
-    test(portNo);
+void signal_callback_handler(int signum) {
+   cout << "Caught signal " << signum << endl;
+   // Gracefully Terminate on signal
+   if (server != nullptr) {
+     server->stop();
+   }
+   exit(signum);
 }
-
 
 void test(uint portNo) {
     SocketAddress sa("localhost", portNo);
     cout << "Port no. to be assigned: " << portNo << endl;
     StreamSocket sock(sa);
     cout << "Welcome to POCO TCP server running on port: " << portNo << ". Enter your string:" << endl;
-    while (1) {
+    while (true) {
       string data;
-      cout << "Input by a person" << endl;
       cin >> data;
       sock.sendBytes(data.data(), (int)data.size());
       char buffer[256] = {0};
       int n = sock.receiveBytes(buffer, sizeof(buffer));
-      cout << "Output by server" << endl;
       cout << string(buffer, n) << endl;
     }
+}
 
-    server.stop();
+int main() {
+    signal(SIGINT, signal_callback_handler);
+    int portNo = 28888;
+    TCPServer localserver(new TCPServerConnectionFactoryImpl<ReverseStringConnection>(), portNo);
+    server = &localserver;
+    localserver.start();
+    test(portNo);
+    while (true);
+    localserver.stop();
 }
